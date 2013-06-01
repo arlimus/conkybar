ff80b6222703d5be933922efe79e37d9eaef870a
Dominik Richter <dominik.richter@gmail.com>
2013-06-01 22:20:13 +0200

    load weather data via script every 3 seconds
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ weatherhelper/cweather.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+get(){
+  field="$1"
+  ret="$(cat ~/.conkycolors/weatherhelper/info 2>/dev/null | grep '^'$field':' | sed 's/'$field': //')"
+  if test -z "$ret"; then
+    echo -n '??'
+  else
+    echo -n "$ret"
+  fi
+}
+echo ''\
+'${voffset 108}${alignc}'$(get 'right_now_c')'°C'\
+'${voffset 42}${goto 35}'$(get 'earlier_c')'°C'\
+'${voffset 0}${goto 112}'$(get 'later_c')'°C'
