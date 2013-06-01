762e27c1371183b7720064bb2c6c5c828fa29deb
Dominik Richter <dominik.richter@gmail.com>
2013-06-01 20:40:01 +0200

    calculate both fahrenheit and celsius temperatures (for current temperature)
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- weatherhelper/weather.com.sh
+++ weatherhelper/weather.com.sh
@@ -16,9 +16,16 @@ mkdir -p "$ICON_DST"
 wget --user-agent="Mozilla" "$TODAY" -O /tmp/weather.today.html
 
 echo -n > "$OUT"
-echo "right_now: $(
-  grep -oP '(?<=<span class="wx-value" itemprop="temperature-fahrenheit">)[0-9]+([.][0-9]+)?' /tmp/weather.today.html
-  )" >> "$OUT"
+
+# get the current temperature
+tmp_f=$( grep -oP '(?<=<span class="wx-value" itemprop="temperature-fahrenheit">)[0-9]+([.][0-9]+)?' /tmp/weather.today.html )
+if test -n "$tmp_f"; then
+  echo "right_now_f: $tmp_f" >> "$OUT"
+  echo "right_now_c: $((($tmp_f-32)*5/9))" >> "$OUT"
+else
+  echo "right_now_f: ??" >> "$OUT"
+  echo "right_now_c: ??" >> "$OUT"
+fi
 
 ICONS_TODAY="$(cat /tmp/weather.today.html | grep "wx-weather-icon" | tail -n3 | grep -oP "[0-9]+[.]png")"
 cp "${ICONS}/$(echo ${ICONS_TODAY}| awk '{print $1}')" "$ICON_DST"/now.png
