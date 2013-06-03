01b4a0acb7dd350e0e1a67284aa68fdbcc4aad1b
Dominik Richter <dominik.richter@gmail.com>
2013-06-03 12:59:06 +0200

    update the network device when conkybar is called
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/conkybar.rb
+++ lib/conkybar.rb
@@ -5,8 +5,8 @@ module Conkybar
   def set_net_device dev, network_file = 'cnetwork'
     f = File::expand_path(File::join(INSTALL_PATH,network_file))
     r = File::read(f).
-      gsub(/(if_up|upspeedgraph|downspeedgraph|addr)\s+[a-z0-9]+/, "\\1 #{dev}").
-      gsub(/(wireless_link_bar\s+[0-9]+,[0-9]+)\s+[a-z0-9]+/, "\\1 #{dev}")
+      gsub(/(if_up|upspeedgraph|downspeedgraph|addr|IP)\s+[a-z]+[0-9]+/, "\\1 #{dev}").
+      gsub(/(wireless_link_bar\s+[0-9]+,[0-9]+)\s+[a-z]+[0-9]+/, "\\1 #{dev}")
     File::write f, r
     puts "done (set net to #{dev})"
   end
